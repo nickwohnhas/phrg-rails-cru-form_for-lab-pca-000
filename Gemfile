16a727c761fd6be2f15833c1652db007ae14f6ea
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-16 20:12:30 -0400

    built a song app

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ group :development, :test do
   gem "byebug"
   gem "capybara"
   gem "database_cleaner"
+  gem "pry"
   gem "rspec-rails"
 end
 
---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,34 @@
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(post_params(:name, :bio))
+    @artist.save
+    # binding.pry
+    redirect_to artist_path(@artist)
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(post_params(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def post_params(*args)
+    params.require(:artist).permit(*args)
+  end
+end
